1db5e425c54cb94be0fe14afc739e85c254e25f4
swaroop <50608646+swaroopsai999@users.noreply.github.com>
2021-03-17 14:40:00 +0530

    Update stop.ps1

--- stop.ps1
+++ stop.ps1
@@ -1,5 +1,5 @@
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
-$VMs = Get-AzureRMVM | where {$_.Tags.Values -like '*6amStop*'}
+$VMs = Get-AzureRMVM | where {$_.Tags.Values -like '*Include*'}
 $VMs | Stop-AzureRMVM -force
 Write-Output $VMs.Name
