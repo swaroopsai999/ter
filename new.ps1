9e0b2352b4ee5cf427abe0dd36b1da6da9858904
swaroop <50608646+swaroopsai999@users.noreply.github.com>
2021-03-17 14:39:19 +0530

    Update new.ps1

--- new.ps1
+++ new.ps1
@@ -1,5 +1,5 @@
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
-$VMs = Get-AzureRMVM | where {$_.Tags.Values -like '*6amStart*'}
+$VMs = Get-AzureRMVM | where {$_.Tags.Values -like '*Include*'}
 $VMs | Start-AzureRMVM
 Write-Output $VMs.Name
